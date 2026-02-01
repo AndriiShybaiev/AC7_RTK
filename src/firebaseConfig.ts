23e1d2ed9790a5f20d0f12dceec69f1b5f0a034d
AndriiShybaiev <60670387+AndriiShybaiev@users.noreply.github.com>
2026-02-01 12:45:41 +0100

    AC 6.2 Integrated Firebase configuration into the app, updated script reference in `index.html`, and added the new Firebase initialization file.

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCU0WPTyqBGvgTZkIoplVUPfee4Zxaln44",
+    authDomain: "ac62-a682c.firebaseapp.com",
+    databaseURL: "https://ac62-a682c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ac62-a682c",
+    storageBucket: "ac62-a682c.firebasestorage.app",
+    messagingSenderId: "574850486529",
+    appId: "1:574850486529:web:dffd5d25e74d2501d58b00"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
